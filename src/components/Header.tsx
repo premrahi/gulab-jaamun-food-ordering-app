19b8efbca242c4606270c67fa02d16bf4160eb71
premrahi <premrahi234@gmail.com>
2026-02-20 14:10:34 +0530

    added NAVLINK to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { useState, useContext } from "react";
-// import { logo_url } from "../utils/constants";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
 import newLogo from "url:../assets/newLogo.png";
@@ -19,19 +18,35 @@ const Header = () => {
   return (
     <div className="flex justify-between shadow-lg bg-white h-22 ">
       <div className="ml-14 justify-center">
-       <Link to="/"><img className="w-54 ml-6 mt-0.5" src={newLogo} alt="Logo" /></Link>
+        <NavLink to="/">
+          <img className="w-54 ml-6 mt-0.5" src={newLogo} alt="Logo" />
+        </NavLink>
       </div>
 
-      <div className="justify-center mr-20 mt-3">
-        <ul className="flex m-4 items-center ">
+      <div className=" flex justify-between mr-20">
+        <ul className="flex m-4 justify-center items-center mx-auto">
           <li className=" font-medium text-gray-500  p-2 px-3 rounded-full   bg-gray-100">
             status:{onlineStatus ? "🟢" : "🔴"}
           </li>
           <li className="pl-6 w-28  font-medium text-center hover:cursor-pointer hover:text-xl overflow-hidden hover:scale-105 transition-transform duration-300">
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-orange-600 font-bold " : "text-gray-700"
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li className="px-2 w-28  font-medium text-center hover:cursor-pointer hover:text-xl overflow-hidden hover:scale-105 transition-transform duration-300">
-            <Link to="/about">Profile</Link>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "text-orange-600 font-bold " : "text-gray-700"
+              }
+            >
+              Profile
+            </NavLink>
           </li>
           {/* <li className="px2 w-28 text-center  font-medium hover:cursor-pointer hover:text-xl overflow-hidden hover:scale-105 transition-transform duration-300">
             <Link to="/contact">Contact Us</Link>
@@ -40,15 +55,24 @@ const Header = () => {
             <Link to="/Grocery">Grocery Store</Link>
           </li> */}
           <li className="px-2 w-28 font-medium hover:cursor-pointer hover:text-xl overflow-hidden hover:scale-105 transition-transform duration-300">
-            <Link to="/cart">
+            <NavLink
+              to="/cart"
+              className={({ isActive }) =>
+                isActive ? "text-orange-600 font-bold " : "text-gray-700"
+              }
+            >
               <span className="bg-yellow-300 rounded-full p-1 m-1 ">
                 ({cartItems.length})
               </span>
               Cart
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <img src={user} alt="user" className="w-10 p-2 bg-gray-100 rounded-2xl cursor-pointer" />
+            <img
+              src={user}
+              alt="user"
+              className="w-10 p-2 bg-gray-100 rounded-2xl cursor-pointer"
+            />
           </li>
           {/* <button
             className="px-6 font-bold border-2 py-4 rounded-lg bg-white hover:cursor-pointer"
