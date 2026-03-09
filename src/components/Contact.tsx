759aabc91bd0961a25bdb0340db583a07a1969d5
premrahi <premrahi234@gmail.com>
2026-03-09 23:34:10 +0530

    fixed contact us page bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,9 @@ const Contact = () => {
 
      <div className="flex md:flex-col justify-center text-center md:m-auto m-4 border bg-amber-200 md:w-200 mt-10 md:mt-10 p-4 font-medium rounded-2xl">
      <h1 className="m-3">contact us page</h1> 
-      <form>
+      <form onSubmit={(e : any ) => {
+        e.preventDefault() ;
+      }}>
         <input
           type="text"
           className="border border-black m-2 p-2 rounded-lg bg-white"
@@ -14,7 +16,7 @@ const Contact = () => {
           className="border border-black m-2 p-2 rounded-lg bg-white"
           placeholder="Message"
         />
-        <button className="m-2 p-2 border border-black bg-white rounded-lg cursor-pointer">
+        <button  className="m-2 p-2 border border-black bg-white rounded-lg cursor-pointer">
           submit
         </button>
       </form>
